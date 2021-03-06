f2eb3e290abd928cbea21aec7aa114667682e442
Radu Gusanu <raduga9199@gmail.com>
2021-03-06 09:49:27 -0500

    Commit

--- src/test/java/com/cybertek/Day06/HR_ORDS_POJO_Test.java
+++ src/test/java/com/cybertek/Day06/HR_ORDS_POJO_Test.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -63,6 +64,9 @@ public class HR_ORDS_POJO_Test extends importsAndURL {
         for (Locations each : locations){
             System.out.println("Version 2: " + each);
         }
+
+
+        assertThat(locations, hasSize(23));
 
 
 
